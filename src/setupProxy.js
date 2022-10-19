8a04fd428711a7a7fecf3c7c6828a99b8757771b
Kim Dong Hun <hunkim98@gmail.com>
2022-10-19 14:27:56 +0900

    customize for github

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const isDevMode = process.env.NODE_ENV === "development";
 
 module.exports = function (app) {
+  const originApiUrl = isDevMode ? "/api" : "/cryptogalaxy/api";
   app.use(
-    "/api",
+    originApiUrl,
     createProxyMiddleware({
       target: "https://upbit-next-proxy.vercel.app/api",
       changeOrigin: true,
